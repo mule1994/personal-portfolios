50fa7d4417acd1d409a3133a56315179b87d8baf
mule1994 <139151708+mule1994@users.noreply.github.com>
2024-08-25 02:26:25 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors :{
+        'neutralSliver':'#F5F7FA',
+        
+        'neutralDGray':'#4D4D4D',
+        'brandPrimary':'#4CAF4F',
+        'neutralGrey':'#717171',
+        
+        
+        }
+        
+    },
+  },
+  plugins: [],
+}
+
